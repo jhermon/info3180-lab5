6f63004ce46ac6a5de65f687402cb4e15c199711
jhermon <suddyjerm@gmail.com>
2020-03-02 22:48:06 -0500

    lab5 vol4

--- app/views.py
+++ app/views.py
@@ -33,12 +33,14 @@ def about():
 def login():
     form = LoginForm()
     if request.method == "POST" and form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
         # change this to actually validate the entire form submission
         # and not just one field
-        if form.username.data and form.password.data:
+        if form.username.data:
             # Get the username and password values from the form.
             user = UserProfile.query.filter_by(username=username, password=password).first()
-
+            
             # using your model, query database for a user based on the username
             # and password submitted. Remember you need to compare the password hash.
             # You will need to import the appropriate function to do so.
@@ -48,8 +50,8 @@ def login():
             # get user id, load into session
             if user is not None and check_password_hash(user.password, password):
                 login_user(user)  
-                flash('You have been successfully logged in')
                 if current_user.is_authenticated:
+                    flash('You have been successfully logged in')
                     return redirect(url_for('secure_page'))
             # remember to flash a message to the user
             return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
