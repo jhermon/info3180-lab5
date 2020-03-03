6d6f70decebd0f7d9c1427a2760b3b584ba8c972
jhermon <suddyjerm@gmail.com>
2020-03-02 22:33:42 -0500

    lab5 vol3

--- app/views.py
+++ app/views.py
@@ -10,6 +10,7 @@ from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user, login_required
 from app.forms import LoginForm
 from app.models import UserProfile
+from werkzeug.security import check_password_hash
 
 
 ###
@@ -31,11 +32,12 @@ def about():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
-    if request.method == "POST":
+    if request.method == "POST" and form.validate_on_submit():
         # change this to actually validate the entire form submission
         # and not just one field
-        if form.username.data:
+        if form.username.data and form.password.data:
             # Get the username and password values from the form.
+            user = UserProfile.query.filter_by(username=username, password=password).first()
 
             # using your model, query database for a user based on the username
             # and password submitted. Remember you need to compare the password hash.
@@ -44,11 +46,28 @@ def login():
             # passed to the login_user() method below.
 
             # get user id, load into session
-            login_user(user)
-
+            if user is not None and check_password_hash(user.password, password):
+                login_user(user)  
+                flash('You have been successfully logged in')
+                if current_user.is_authenticated:
+                    return redirect(url_for('secure_page'))
             # remember to flash a message to the user
             return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
     return render_template("login.html", form=form)
+
+
+@app.route('/secure-page')
+@login_required
+def secure_page():
+    return render_template('secure_page.html')
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash('logged out.')
+    return redirect(url_for('home'))
 
 
 # user_loader callback. This callback is used to reload the user object from
